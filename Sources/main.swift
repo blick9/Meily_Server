59a902f10a82b59460f598da5ce77a22a7578405
Koo Chanwoo <febrix9@gmail.com>
2017-04-06 19:46:34 +0900

    Complete prepare networking

--- Sources/main.swift
+++ Sources/main.swift
@@ -34,10 +34,12 @@ router.post("/api/user/register") { (request, response, next) in
 
     let isDuplication = DataBase.sharedInstance.validDuplication(email: validDicData["email"]!)
     if isDuplication == true {
-        response.send("이미 존재하는 이메일 입니다.")
+//        response.send("이미 존재하는 이메일 입니다.")
+        response.send(status: .conflict)
     } else {
         let isResisterWorking = DataBase.sharedInstance.saveUserData(Email: validDicData["email"]!, Password: validDicData["password"]!)
-        response.send("회원가입 성공!")
+//        response.send("회원가입 성공!")
+        response.send(status: .OK)
     }
     next()
 }
@@ -50,33 +52,33 @@ router.post("/api/user/login") { (request, response, next) in
     
     let isDuplication = DataBase.sharedInstance.validDuplication(email: validDicData["email"]!)
     if isDuplication == false {
-        response.send("존재하지 않는 이메일입니다.")
+//        response.send("존재하지 않는 이메일입니다.")
+        response.send(status: .notFound)
     } else if DataBase.sharedInstance.login(Email: validDicData["email"]!, Password: validDicData["password"]!) == false {
-        response.send("비밀번호가 맞지 않습니다.")
+//        response.send("비밀번호가 맞지 않습니다.")
+        response.send(status: .notAcceptable)
     } else {
-        response.send("로그인 성공!")
+//        response.send("로그인 성공!")
+        response.send(status: .OK)
     }
     next()
 }
 
 
 // Get일때 url로 값 넘기는 방법은?
-router.get("/api/user/list") { (request, response, next) in
-    var data = Data()
-    let readData = try request.read(into: &data)
-    let emailData = String(data : data, encoding: .utf8)
-    print(request, emailData)
+router.get("/api/user/list/:email") { (request, response, next) in
+    let emailData = request.parameters["email"] ?? ""
     
-    if let validData = emailData {
-        let isDuplication = DataBase.sharedInstance.validDuplication(email: validData)
-        if isDuplication == true {
-            try response.send("이미 존재하는 E-mail 입니다.").end()
-        } else {
-            try response.send("중복된 E-mail 없음").end()
-        }
-        Log.info("\(isDuplication)")
-        next()
+    let isDuplication = DataBase.sharedInstance.validDuplication(email: emailData)
+    if isDuplication == true {
+//        try response.send("이미 존재하는 E-mail 입니다.").end()
+        response.send(status: .conflict)
+    } else {
+//        try response.send("중복된 E-mail 없음").end()
+        response.send(status: .accepted)
     }
+    Log.info("\(isDuplication)")
+    next()
 }
 
 router.get("/api/user/alllist") { (request, response, next) in
