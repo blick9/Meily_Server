ba2570ed57a0670369e18a61fc7b7bde7c0558b8
brix <febrix9@gmail.com>
2017-09-05 23:20:06 +0900

    Apply .gitignore to Repository

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Kitura-Maily", 
+    name: "Meily-Server", 
     dependencies: [
       .Package(url:"https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
       .Package(url:"https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1)
